8582ccd78886eba211a4cf2e4289fa322bfc7864
Daniele Ballinari <daniele.ballinari@gmail.com>
2020-03-24 12:00:49 +0100

    better rescaling factor construction

--- Code/GoogleTrendsScraper.py
+++ Code/GoogleTrendsScraper.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import time
 import numpy as np
+import math
 from selenium import webdriver
 from selenium.common import exceptions
 import os
@@ -22,15 +23,17 @@ def merge_two_trends(data_first, data_second):
     """
 
     # In some cases, GoogleTrends reports very small values as '<1'. These values are replaced by 0.5 and the data-type
-    # changed to 'float'. Note that the first column contains the dates as strngs and therefore is not modified.
-    data_first.iloc[:, 1:] = data_first.iloc[:, 1:].replace('<1', 0.5).astype('float')
-    data_second.iloc[:, 1:] = data_second.iloc[:, 1:].replace('<1', 0.5).astype('float')
-    # Calculate the scaling factor, by computing a ratio of the two overlapping observations
-    factor = data_first.iloc[-1, 1:] / data_second.iloc[0, 1:]
+    # changed to 'float'. Note that the first column contains the dates as strings and therefore is not modified.
+    data_first = data_first.replace('<1', 0.5).astype('float')
+    data_second = data_second.replace('<1', 0.5).astype('float')
+    # Find overlapping observations:
+    common_obs = data_first.index.intersection(data_second.index)
+    # Calculate the scaling factor, by computing a ratio of the overlapping observations
+    factor = np.nanmedian(data_first.loc[common_obs, :]/data_second.loc[common_obs, :])
     # Rescale the second data set with the factor, such that all observations are on a comparable scale
-    data_second.iloc[:, 1:] = data_second.iloc[:, 1:] * factor
-    # Remove the first observation of the second data set, and append it to the first data set
-    return data_first.append(data_second.iloc[1:, :], ignore_index=True)
+    data_second = data_second * factor
+    # Combine the data sets and for overlapping observations, keep only the first one
+    return data_first.combine_first(data_second)
 
 
 def merge_data(data_list, keywords):
@@ -48,11 +51,19 @@ def merge_data(data_list, keywords):
     # Iteratively merge the DataFrame objects in the list of data
     data = reduce((lambda x, y: merge_two_trends(x, y)), data_list)
     # Find the maximal value across keywords and time
-    max_value = data.iloc[:, 1:].max().max()
+    max_value = data.max().max()
     # Rescale the trends by the maximal value, i.e. such that the largest value across keywords and time is 100
-    data.iloc[:, 1:] = 100 * data.iloc[:, 1:] / max_value
+    data = 100 * data / max_value
     # Rename the columns
-    data.columns = ['Date'] + keywords
+    data.columns = keywords
+    return data
+
+
+def merge_two_keyword_chunks(data_first, data_second):
+    common_keyword = data_first.columns.intersection(data_second.columns)[0]
+    scaling_factor = np.nanmedian(data_first[common_keyword] / data_second[common_keyword])
+    data_second = data_second.apply(lambda x: x * scaling_factor)
+    data = pd.merge(data_first, data_second.drop(common_keyword, axis=1), left_index=True, right_index=True)
     return data
 
 
@@ -70,16 +81,41 @@ def adjust_date_format(date, format_in, format_out):
     return datetime.strptime(date, format_in).strftime(format_out)
 
 
-class GoogleTrendsScraper:
-    def __init__(self, sleep=1, path_driver=None, headless=True, date_format='%Y-%m-%d'):
-        """
+def get_chunks(list_object, chunk_size):
+    """
+    Generator that divides a list into chunks
+    Args:
+        list_object: iterable
+        chunk_size: size of each chunk as an integer
 
+    Returns: iterable in chunks
+
+    """
+    size = len(list_object)
+    if size <= chunk_size:
+        yield list_object
+    chunks_nb = math.ceil(size / chunk_size)
+    iter_ints = range(0, chunks_nb)
+    for i in iter_ints:
+        j = i * chunk_size
+        if i + 1 < chunks_nb:
+            k = j + chunk_size
+            yield list_object[max(j - 1, 0):k]
+        else:
+            yield list_object[max(j - 1, 0):]
+
+
+class GoogleTrendsScraper:
+    def __init__(self, sleep=1, path_driver=None, headless=True, date_format='%Y-%m-%d', n_overlap=5):
+        """
+        Constructor of the Google-Scraper-Class
         Args:
             sleep: integer number of seconds where the scraping waits (avoids getting blocked and gives the code time
                     to download the data
             path_driver: path as string to where the chrome driver is located
             headless: boolean indicating whether the browser should be displayed or not
             date_format: format in which the date-strings are passed to the object
+            n_overlap: integer number of overlapping observations used to rescale multiple sub-period trends
         """
         # Current directory
         self.dir = os.getcwd()
@@ -102,6 +138,8 @@ class GoogleTrendsScraper:
         self.date_format = date_format
         # Format of dates used by google
         self._google_date_format = '%Y-%m-%d'
+        # Number of overlapping observations:
+        self.n_overlap = n_overlap
         # Lunch the browser
         self.start_browser()
 
@@ -153,7 +191,7 @@ class GoogleTrendsScraper:
 
         """
         # If only a single keyword is given, i.e. as a string and not as a list, put the single string into a list
-        if keywords is not list:
+        if not isinstance(keywords, list):
             keywords = [keywords]
         # Convert the date strings to Google's format:
         start = adjust_date_format(start, self.date_format, self._google_date_format)
@@ -161,12 +199,16 @@ class GoogleTrendsScraper:
         # Create datetime objects from the date-strings:
         start_datetime = datetime.strptime(start, self._google_date_format)
         end_datetime = datetime.strptime(end, self._google_date_format)
-        # Iterate over the URLs of the sub-periods and retrieve the Google Trend data for each
-        data_list = []
-        for url in self.create_urls(keywords, start_datetime, end_datetime, region):
-            data_list.append(self.get_data(url))
-        # Merge the so obtained set of DataFrames to a single DataFrame
-        data = merge_data(data_list, keywords)
+        data_keywords_list = []
+        for keywords_i in get_chunks(keywords, 5):
+            # Iterate over the URLs of the sub-periods and retrieve the Google Trend data for each
+            data_time_list = []
+            for url in self.create_urls(keywords_i, start_datetime, end_datetime, region):
+                data_time_list.append(self.get_data(url))
+            # Merge the so obtained set of DataFrames to a single DataFrame
+            data_keywords_list.append(merge_data(data_time_list, keywords_i))
+
+        data = reduce((lambda x, y: merge_two_keyword_chunks(x, y)), data_keywords_list)
         return data
 
     def create_urls(self, keywords, start, end, region=None):
@@ -191,7 +233,7 @@ class GoogleTrendsScraper:
 
         for x in range(int(num_subperiods)):
             start_sub = start + timedelta(days=x * length_subperiods)
-            end_sub = start + timedelta(days=(x + 1) * length_subperiods)
+            end_sub = start + timedelta(days=(x + 1) * length_subperiods + 4)
             # It might be that the end of the sub-period exceeds the actual end of the trend period. In that case, we
             # let this period end at the date defined by 'end'
             if end_sub > end:
@@ -226,7 +268,8 @@ class GoogleTrendsScraper:
                 # reduce the likelihood that Google detects us as a scraper
                 time.sleep(self.sleep + 2 * np.random.rand(1))
                 # Try to find the button and click it
-                button = self.browser.find_element_by_css_selector('.widget-actions-item.export')
+                line_chart = self.browser.find_element_by_css_selector("widget[type='fe_line_chart']")
+                button = line_chart.find_element_by_css_selector('.widget-actions-item.export')
                 button.click()
             except exceptions.NoSuchElementException:
                 # If the button cannot be found, try again (load page, ...)
@@ -235,6 +278,8 @@ class GoogleTrendsScraper:
         time.sleep(self.sleep)
         # Load the data from the csv-file as pandas.DataFrame object
         data = pd.read_csv(self.filename, skiprows=1)
+        # Set date as index:
+        data = data.set_index("Day")
         # Sleep again
         time.sleep(self.sleep)
         # Delete the file
