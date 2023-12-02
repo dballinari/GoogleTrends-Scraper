614296626a32ce460a095b1e1697c2c412be35d6
Daniele Ballinari <daniele.ballinari@gmail.com>
2023-12-02 13:44:05 +0100

    Update modules and use tmp dir

--- example_script.py
+++ example_script.py
@@ -4,4 +4,6 @@ gts = GoogleTrendsScraper(
     sleep=2, path_driver='C:/Users/danie/chromedriver/chromedriver.exe', headless=False)
 data = gts.get_trends('foo', '2018-07-02', '2019-04-02', 'US')
 
+print(data)
+
 del gts
